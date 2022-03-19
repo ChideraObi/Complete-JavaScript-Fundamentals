4d9163fe54affa54bbf2b67ec6fe11db029ffe82
Chidera Obinali <chideraobinali@Chideras-MBP.attlocal.net>
2022-03-19 16:49:31 -0400

    Created js file and completed first assignment

---
+++ Assignments/assignments.js
@@ -0,0 +1,11 @@
+/* 1. Declare variables called 'country', 'continent' and 'population' and
+assign their values according to your own country (population in millions)
+2. Log their values to the console */
+
+let country = 'United States of America';
+let continent = 'North America';
+let population = '332,278,200';
+
+console.log(country);
+console.log(continent);
+console.log(population);
